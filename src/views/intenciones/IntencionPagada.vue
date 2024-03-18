c6ccc345de1235a6f04a41274360ddff34a031cf
WilGero <wil.gero.2020@gmail.com>
2024-03-18 11:07:22 -0400

    Se implemento la operacion de actualizar los estados de pago a verdadero, al momento de ingresar a la vista , Intencion pagada

--- src/views/intenciones/IntencionPagada.vue
+++ src/views/intenciones/IntencionPagada.vue
@@ -20,38 +20,82 @@ export default {
         id: null,
         estado_pago: 0,
       },
+      intenciones: [],
     };
   },
-  created(){
-    // this.actualizarEstadoPago();
+  created() {
+    this.getIntenciones();
+    this.actualizarEstadoPagoLista();
   },
   methods: {
-    async actualizarEstadoPago() {
-      this.form = {
-        id: this.$route.params.id,
-        estado_pago: 1,
-      };
-      console.log(this.form);
+    async getIntenciones() {
       await this.axios
-        .patch("/intenciones/actualizar-estado", this.form)
+        .get("/lista-intenciones/encontrar/" + this.$route.params.listaId)
         .then((response) => {
           // Manejar la respuesta exitosa
-          console.log("estado de pago actualizada exitosamente ", response.data.data);
+          this.intenciones = response.data.data;
+          console.log(this.intenciones);
         })
         .catch((error) => {
           // Manejar errores
-          console.error("Error al actualizar misa:", error);
+          console.error("Error al listar las intenciones:", error);
         });
     },
-    verDetalle(){
+    async actualizarEstadoPagoLista() {
+      this.form = {
+        id: this.$route.params.listaId,
+        estado_pago: 1, //indica estado pagado
+      };
+      console.log(this.form);
+      await this.axios
+        .patch("/lista-intenciones/actualizar-estado", this.form)
+        .then((response) => {
+          // Manejar la respuesta exitosa
+          console.log(
+            "estado de pago de la lista actualizada exitosamente ",
+            response.data.data
+          );
+          this.actualizarEstadoPagoIntencion();
+        })
+        .catch((error) => {
+          // Manejar errores
+          console.error("Error al actualizar estado:", error);
+        });
+    },
+    async actualizarEstadoPagoIntencion() {
+      this.form = {
+        id: this.$route.params.listaId,
+        estado_pago: 1, //indica estado pagado
+      };
+      for (var i = 0; i < this.intenciones.length; i++) {
+        this.form = {
+          id: this.intenciones[i].id,
+          estado_pago: 1,
+        };
+        console.log(this.form);
+        await this.axios
+          .patch("/intenciones/actualizar-estado", this.form)
+          .then((response) => {
+            // Manejar la respuesta exitosa
+            console.log(
+              "estado de pago actualizada exitosamente ",
+              response.data.data
+            );
+          })
+          .catch((error) => {
+            // Manejar errores
+            console.error("Error al actualizar estado:", error);
+          });
+      }
+    },
+    verDetalle() {
       this.$router.push({
-        name:'detalleIntencion',
-        params:{
-          listaId:this.$route.params.listaId
-        }
-      })
-    }
-
+        name: "detalleIntencion",
+        params: {
+          listaId: this.$route.params.listaId,
+        },
+      });
+    },
   },
 };
 </script>
