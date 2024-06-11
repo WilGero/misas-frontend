1c737842b9ace5ac85b0ee5546e12d939bcd8c7a
WilGero <wil.gero.2020@gmail.com>
2024-06-11 16:29:03 -0400

    Se implemento en la vista de asistencia de una clase, el control para el maximo de permisos y faltas

--- src/views/catequesis/clases/AsistenciaClase.vue
+++ src/views/catequesis/clases/AsistenciaClase.vue
@@ -5,7 +5,9 @@
         <i class="fas fa-arrow-left"></i> Atrás
       </button>
     </div>
-    <button class="btn btn-success " @click="exportTableToExcel"><i class="fas fa-file-excel"></i> Excel</button>
+    <button class="btn btn-success" @click="exportTableToExcel">
+      <i class="fas fa-file-excel"></i> Excel
+    </button>
     <h2>Catequesis de {{ nombre }}</h2>
     <span class="fs-3 text-start me-5"
       ><strong>Tema:</strong> {{ temaClase }}</span
@@ -15,12 +17,12 @@
     }}</span>
     <h3>Lista de Catecumenos</h3>
     <div class="table-responsive">
-      <table ref="table" class="table table-bordered  table-hover ">
+      <table ref="table" class="table table-bordered table-hover">
         <thead class="table-success">
           <tr>
             <th>Nro</th>
-            <th class="ps-4 text-start ">Nombres</th>
-            <th class="ps-4 text-start ">Apellidos</th>
+            <th class="ps-4 text-start">Nombres</th>
+            <th class="ps-4 text-start">Apellidos</th>
             <th>Asistencia</th>
             <th>Estado</th>
           </tr>
@@ -36,26 +38,30 @@
             <td>
               <section class="btn-group">
                 <button
-                  @click="actualizarAsistencia(item.id, asistencias[0].id)"
+                  @click="actualizarAsistencia(item, asistencias[0].id)"
                   class="btn btn-success btn-attendance"
                 >
                   <i class="fas fa-check"></i>
                 </button>
                 <button
-                  @click="actualizarAsistencia(item.id, asistencias[1].id)"
+                  @click="actualizarAsistencia(item, asistencias[1].id)"
                   class="btn btn-warning btn-attendance"
                 >
                   <i class="fas fa-clock"></i>
                 </button>
                 <button
-                  @click="actualizarAsistencia(item.id, asistencias[2].id)"
+                  @click="handleButtonClick(item, 2)"
                   class="btn btn-info btn-attendance"
+                  :data-bs-toggle="item.max_permiso === 0 ? 'modal' : ''"
+                  :data-bs-target="item.max_permiso === 0 ? '#mi-modal' : ''"
                 >
                   <i class="fas fa-user-clock"></i>
                 </button>
                 <button
-                  @click="actualizarAsistencia(item.id, asistencias[3].id)"
+                  @click="handleButtonClick(item, 3)"
                   class="btn btn-danger btn-attendance"
+                  :data-bs-toggle="item.max_falta === 0 ? 'modal' : ''"
+                  :data-bs-target="item.max_falta === 0 ? '#mi-modal' : ''"
                 >
                   <i class="fas fa-times"></i>
                 </button>
@@ -69,13 +75,28 @@
         </tbody>
       </table>
     </div>
+    <div class="modal fade" id="mi-modal" data-bs-backdrop="static">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h3 class="modal-title">Alerta!!!</h3>
+            <button class="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+          <div class="modal-body">
+            <div class="alert alert-danger alert-dismissible m-4">
+              <span>Ya se alcanzo el número máximo de {{ message }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import moment from "moment";
-import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 export default {
   data() {
     return {
@@ -93,6 +114,7 @@ export default {
       temaClase: null,
       fechaHoraClase: null,
       nombre: null,
+      message: null,
     };
   },
   created() {
@@ -100,8 +122,18 @@ export default {
     this.getCatecumenosClase();
     this.getClase();
   },
-
   methods: {
+    handleButtonClick(item, idAsistencia) {
+      console.log(`Este es el item: ${item}`);
+      if (item.max_permiso === 0) {
+        this.message = "Permisos";
+      } else if (item.max_falta === 0) {
+        this.message = "Faltas";
+      } else {
+        this.actualizarAsistencia(item, this.asistencias[idAsistencia].id);
+        // El modal se abrirá automáticamente debido a los atributos de Bootstrap
+      }
+    },
     exportTableToExcel() {
       // Obtener la referencia de la tabla
       const table = this.$refs.table;
@@ -110,10 +142,13 @@ export default {
       const workbook = XLSX.utils.table_to_book(table, { sheet: "Sheet1" });
 
       // Generar un archivo Excel binario
-      const wbout = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+      const wbout = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
 
       // Guardar el archivo usando FileSaver
-      saveAs(new Blob([wbout], { type: 'application/octet-stream' }), 'tabla.xlsx');
+      saveAs(
+        new Blob([wbout], { type: "application/octet-stream" }),
+        "tabla.xlsx"
+      );
     },
     formatDatetimeWithMonthInLetters(datetime) {
       return moment(datetime)
@@ -162,28 +197,57 @@ export default {
           console.error("Error al encontrar la clase:", error);
         });
     },
+    async actualizarAsistencia(item, asistencia) {
+      try {
+        console.log(
+          `Permisos actuales: ${item.max_permiso}, Faltas actuales: ${item.max_falta}`
+        );
+        if (asistencia === 3) {
+          if(item.asistencia_id !== 3){
+            item.max_permiso -= 1;
+          }
+        } else if (asistencia === 4) {
+          item.max_falta -= 1;
+        } else {
+          if (item.asistencia_id === 3) {
+            item.max_permiso += 1;
+          }else if(item.asistencia_id === 4){
+            item.max_falta += 1;
+          }
+        }
+        const maxResponse = await this.axios.put(
+          "/catecumenos/actualizar-max",
+          {
+            id: item.catecumeno_id,
+            max_permiso: item.max_permiso,
+            max_falta: item.max_falta,
+          }
+        );
+        console.log(
+          "Permisos y faltas actualizados exitosamente",
+          maxResponse.data.data
+        );
 
-    actualizarAsistencia(id, asistencia) {
-      this.axios
-        .put("/catecumenos-clase/actualizar-asistencia", {
-          id: id,
-          asistencia_id: asistencia,
-        })
-        .then((response) => {
-          // Manejar la respuesta exitosa
-          console.log(
-            "asistencia del catecumeno actualizada exitosamente ",
-            response.data.data
-          );
-          this.getCatecumenosClase();
-        })
-        .catch((error) => {
-          // Manejar errores
-          console.error(
-            "Error al actualizar la asistencia del catecumeno:",
-            error
-          );
-        });
+        const asistenciaResponse = await this.axios.put(
+          "/catecumenos-clase/actualizar-asistencia",
+          {
+            id: item.id,
+            asistencia_id: asistencia,
+          }
+        );
+
+        console.log(
+          "Asistencia actualizada exitosamente",
+          asistenciaResponse.data.data
+        );
+
+        await this.getCatecumenosClase();
+      } catch (error) {
+        console.error(
+          "Error al actualizar la asistencia del catecumeno:",
+          error
+        );
+      }
     },
     irAtras() {
       this.$router.go(-1);
