3b44423907077395cab454d753ea195f8b982c83
WilGero <wil.gero.2020@gmail.com>
2024-09-02 19:43:09 -0400

    Se implemento la funcion de eliminar un examen en la vista de examenes

--- src/views/catequesis/examen/IndexExamenes.vue
+++ src/views/catequesis/examen/IndexExamenes.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+  examenId: {{ examenId }}
     <div class="row">
       <div class="col">
         <h2 class="fs-1">Examenes</h2>
@@ -32,10 +33,21 @@
                   <button class="btn btn-primary">
                     <i class="fas fa-file-alt"></i>
                   </button>
-                  <router-link :to="{name:'editarExamen',params:{examenId:item.id}}" class="btn btn-warning">
+                  <router-link
+                    :to="{
+                      name: 'editarExamen',
+                      params: { examenId: item.id },
+                    }"
+                    class="btn btn-warning"
+                  >
                     <i class="fas fa-edit"></i>
                   </router-link>
-                  <button class="btn btn-danger">
+                  <button
+                    class="btn btn-danger"
+                    @click="guardarIdExamen(item.id)"
+                    data-bs-toggle="modal"
+                    data-bs-target="#mi-modal"
+                  >
                     <i class="fas fa-trash"></i>
                   </button>
                 </div>
@@ -43,6 +55,43 @@
             </tr>
           </tbody>
         </table>
+        <!-- modal para eliminar examen -->
+        <div class="modal" id="mi-modal" data-bs-backdrop="static">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h3>Alerta!!!</h3>
+                <button class="btn-close" data-bs-dismiss="modal"></button>
+              </div>
+              <div class="modal-body">
+                <div
+                  v-if="mostrarAlerta"
+                  class="alert alert-danger alert-dismissible m-4"
+                >
+                  <span>{{ mensaje }}</span>
+                </div>
+                <p class="fw-bold text-danger" v-else>
+                  ¿Esta seguro de eliminar el examen?
+                </p>
+              </div>
+              <div class="modal-footer">
+                <button
+                  class="btn btn-secondary"
+                  @click="cerrarModal"
+                  data-bs-dismiss="modal"
+                >
+                  Cerrar</button
+                ><button
+                  v-if="!mostrarAlerta"
+                  class="btn btn-danger"
+                  @click="eliminarExamen"
+                >
+                  Eliminar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -54,6 +103,10 @@ export default {
   data() {
     return {
       examenes: [],
+      catecumenosExamen:[],
+      mostrarAlerta: false,
+      mensaje: null,
+      examenId:null
     };
   },
   created() {
@@ -71,6 +124,42 @@ export default {
     },
     formatearFecha(fecha) {
       return moment(fecha).locale("es").format("D [de] MMMM [del] YYYY");
+    },
+    async getExamenCatecumenos() {
+      try {
+        const response = await this.axios.get(
+          `/catecumenos-examen/encontrar/${this.examenId}`
+        );
+        this.catecumenosExamen = response.data.data;
+        console.log(this.catecumenosExamen);
+      } catch (error) {
+        console.error("Error al encontrar catecumenos en el examen:", error);
+      }
+    },
+    guardarIdExamen(idExamen){
+      this.examenId=idExamen;
+      setTimeout(() => {
+          this.getExamenCatecumenos();
+        }, 500);
+      this.mostrarAlerta=false;
+    },
+    async eliminarExamen() {
+      if (this.catecumenosExamen.length > 0) {
+        this.mostrarAlerta = true;
+        this.mensaje =
+          "No se puede elimina el examen, porque contiene notas registradas";
+        return;
+      }
+
+      try {
+        await this.axios.delete(`/examenes/borrar/${this.examenId}`);
+        console.log("Examen eliminada con éxito");
+
+        this.mostrarAlerta = true;
+        this.mensaje = "Examen eliminado con éxito";
+      } catch (error) {
+        console.error("Error al eliminar el examen:", error);
+      }
     },
   },
 };
