fd1c16d9fe3489162ba13ba1015e61b8306469d0
WilGero <wil.gero.2020@gmail.com>
2025-04-05 00:10:09 -0400

    Se cambia el select option de gestion a la vista IndexCatequesis

--- src/views/catequesis/catequesis/IndexCatequesis.vue
+++ src/views/catequesis/catequesis/IndexCatequesis.vue
@@ -1,26 +1,57 @@
 <template>
   <div class="container mt-4">
     <div class="row p-3">
-      <section
-        class="col-12 align-items-center text-bg-primary bg-opacity-75"
-      >
-        <h2 class="fs-1 ">Catequesis</h2>
+      <section class="col-12 align-items-center text-bg-primary bg-opacity-75">
+        <h2 class="fs-1">Catequesis</h2>
       </section>
-      <!-- boton para agregar -->
-      <section class="col-md-1 col-3 mx-auto bg-light bg-opacity-75 rounded py-2">
-         <button class="btn btn-outline-success" @click="agregarCatequesis">Agregar</button>
-       </section>
+      <div>
+        <div class="row bg-light bg-opacity-75 rounded py-2">
+          <div class="col-6">
+            <div class="row justify-content-center">
+              <label
+                for="selectGestion"
+                class="col-3 form-label p-2 text-bg-dark bg-opacity-75 rounded rounded-end-0"
+                >Gestión:</label
+              >
+              <div class="col-4 p-0">
+                <select
+                  v-model="selectGestion"
+                  id="selectGestion"
+                  class="form-select rounded-start-0"
+                >
+                  <option
+                    v-for="item in gestiones"
+                    :key="item.id"
+                    :value="item.id"
+                  >
+                    {{ item.gestion }}
+                  </option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <!-- boton para agregar -->
+          <section class="col-6">
+            <button class="btn btn-outline-success" @click="agregarCatequesis">
+              Agregar
+            </button>
+          </section>
+        </div>
+      </div>
     </div>
     <div class="row">
-      <div class="col-6" v-for="item in catequesis" :key="item.id">
+      <div class="col-6 mb-3" v-for="item in catequesis" :key="item.id">
         <div class="card shadow h-100">
           <div class="card-body">
             <h5 class="card-title fw-bolder fs-4">{{ item.nombre }}</h5>
             <p class="card-text">{{ item.requisitos }}</p>
- 
-            <router-link class="btn btn-primary" :to="{ name: 'opciones',params:{catequesisId:item.id} }"
-                > Seleccionar</router-link
-              >
+
+            <router-link
+              class="btn btn-primary"
+              :to="{ name: 'opciones', params: { catequesisId: item.id } }"
+            >
+              Seleccionar</router-link
+            >
           </div>
         </div>
       </div>
@@ -29,7 +60,6 @@
 </template>
   
   <script>
-
 export default {
   setup() {
     return {};
@@ -37,15 +67,16 @@ export default {
   data() {
     return {
       catequesis: [],
+      gestiones: [],
       searchQuery: "", // Añadir searchQuery al estado
+      selectGestion: null,
     };
   },
   created() {
     this.getCatequesis();
+    this.getGestion();
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
     async getCatequesis() {
       await this.axios
@@ -60,20 +91,36 @@ export default {
           console.error("Error al listar catequesis:", error);
         });
     },
-    async agregarCatequesis(){
-      this.$router.push({name:"agregarCatequesis"})
-    }    
+    async agregarCatequesis() {
+      this.$router.push({ name: "agregarCatequesis" });
+    },
+    //metodo que lista a todas las gestiones
+    async getGestion() {
+      await this.axios
+        .get("/gestion/listado")
+        .then((response) => {
+          // Manejar la respuesta exitosa
+          this.gestiones = response.data.data;
+          console.log(this.gestiones);
+          //estamos recuperando la gestion 2025 por defecto
+          this.selectGestion = this.gestiones[1].id;
+        })
+        .catch((error) => {
+          // Manejar errores
+          console.error("Error al listar las gestiones:", error);
+        });
+    },
   },
 };
 </script>
   
   <style>
-  .pos-a{
-    position: absolute;
-  }
-  .pos-r{
-    position: relative;
-  }
+.pos-a {
+  position: absolute;
+}
+.pos-r {
+  position: relative;
+}
 /* Estilos personalizados */
 .table-responsive {
   overflow-x: auto;
--- src/views/catequesis/catequesis/SelectCatequesis.vue
+++ src/views/catequesis/catequesis/SelectCatequesis.vue
@@ -1,30 +1,19 @@
 <template>
   <div class="container mt-4">
+    {{ this.catequesis_gestion }}
     <div class="col-12 p-3">
       <section
         class="d-flex justify-content-around align-items-center text-bg-primary bg-opacity-75"
       >
-        <h2 class="fs-1">{{ nameCateqesis }}</h2>
-        {{ selectGestion }}
+      <h2 class="fs-1">{{ nameCateqesis }}</h2>
       </section>
       <!-- Barra de búsqueda -->
       <div class="row mb-3 justify-content-center">
         <label
           for="selectGestion"
-          class="col-3 form-label p-2 text-bg-dark bg-opacity-75 rounded"
+          class="col-3 form-label p-2 text-bg-dark bg-opacity-75 rounded rounded-end-0"
           >Selecciona la Gestión:</label
         >
-        <div class="col-4 p-0">
-          <select
-            v-model="selectGestion"
-            id="selectGestion"
-            class="form-select"
-          >
-            <option v-for="item in gestiones" :key="item.id" :value="item.id">
-              {{ item.gestion }}
-            </option>
-          </select>
-        </div>
       </div>
     </div>
     <div class="row">
@@ -33,7 +22,9 @@
           <div class="card-body">
             <h5 class="card-title fw-bolder fs-4">{{ item }}</h5>
             <p class="card-text"></p>
-            <button class="btn btn-primary" @click="catecumenos()">Seleccionar</button>
+            <button class="btn btn-primary" @click="catecumenos()">
+              Seleccionar
+            </button>
           </div>
         </div>
       </div>
@@ -55,32 +46,63 @@ export default {
         "Estadísticas",
       ],
       gestiones: [],
+      catequesis_gestion: null,
       catequesis: null,
       nameCateqesis: null,
       selectGestion: null, // Añadir searchQuery al estado
     };
   },
   created() {
-    this.getGestion();
     this.encontrarCatequesis();
   },
-  computed: {},
+  // mounted() {
+  //   this.encontrarCatequesisGestion();
+  // },
+  updated(){
+    // this.encontrarCatequesisGestion();
+  },
+  computed: {
+  },
   methods: {
-    async getGestion() {
+        //metodo que encuentra una catequesis_gestiones segun el catequesis_id y la gestion_id
+    async encontrarCatequesisGestion() {
       await this.axios
-        .get("/gestion/listado")
+        .get(
+          `/catequesis-gestion/encontrar/${this.$route.params.catequesisId}/${this.selectGestion}`
+        )
         .then((response) => {
           // Manejar la respuesta exitosa
-          this.gestiones = response.data.data;
-          console.log(this.gestiones);
-          //estamos recuperando la gestion 2025 por defecto
-          this.selectGestion = this.gestiones[1].id;
+          this.catequesis_gestion = response.data.data;
+          console.log(this.catequesis_gestion);
+          if(this.catequesis_gestion===undefined){
+            this.agregarCatequesisGestion();
+          }
         })
         .catch((error) => {
           // Manejar errores
           console.error("Error al listar las gestiones:", error);
         });
     },
+    //agregar catequesis_gestiones
+    async agregarCatequesisGestion(){
+      await this.axios
+        .get("/catequesi-gestion/agregar",
+          {
+            catequesis_id:this.$route.params.catequesisId,
+            gestion_id:this.selectGestion
+          }
+        )
+        .then((response) => {
+          // Manejar la respuesta exitosa
+          console.log(response);
+          this.encontrarCatequesis();
+        })
+        .catch((error) => {
+          // Manejar errores
+          console.error("Error al listar la catequesis:", error);
+        });
+    }, 
+    //metodo que encuentra la catequesis por el id
     async encontrarCatequesis() {
       await this.axios
         .get("/catequesis/encontrar/" + this.$route.params.catequesisId)
